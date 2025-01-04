a45e4a9ab4ba16156de4c6e4126b3017e7c663c0
fruit <nico.travert@gmail.com>
2025-01-04 19:03:50 +0100

    fix ???

--- actions.py
+++ actions.py
@@ -2,24 +2,14 @@ import projet
 import random
 import noms
 
-<<<<<<< HEAD
-from projet import Personne, Roturier, Soldat, Ecclesiastique, Noble
-=======
 from projet import Personne, Roturier, Soldat, Ecclesiastique, Noble, Village
->>>>>>> 796d987 (branche)
 
 cout_action=0
 
 
-<<<<<<< HEAD
-def creer_personne(statut, player = None, paysan = True):
-    if statut == "roturier":
-        return Roturier("paysan" if paysan else "artisan")
-=======
 def creer_personne(statut: str, player = None, paysan = True):
     if statut == "roturier":
         return Roturier()
->>>>>>> 796d987 (branche)
     elif statut == "soldat":
         return Soldat()
     elif statut == "ecclesiastique":
@@ -46,15 +36,8 @@ def creer_village(zone, player, init = False): #zone est une case
     if not init:
         player.actions -= 1
         player.village.chef.ressources -= 10
-<<<<<<< HEAD
-    chef = creer_personne("noble", player)
-    village = projet.Village(zone, chef)
-    village.habitants = [chef]+[creer_personne("roturier") for i in range(4)]
-    chef.village = village
-=======
 
     village = Village(zone, player)
->>>>>>> 796d987 (branche)
     zone.type = "village"
     zone.master = village
     player.fief.append(village)
